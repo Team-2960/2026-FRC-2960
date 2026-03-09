c5eee5b9e2c27b7dd5a89e057dff39a4447c5c4e
Hangyul Kim <hangyulheaven@gmail.com>
2026-03-09 18:39:10 -0400

    Fixed Merge error

--- src/main/java/frc/robot/subsystems/IntakeAngle.java
+++ src/main/java/frc/robot/subsystems/IntakeAngle.java
@@ -276,16 +276,6 @@ public class IntakeAngle extends SubsystemBase {
         }
     }
 
-
-    /**
-     * Create a Quasistatic SysId command
-     * @param direction direction of the command
-     * @return  Quasistatic SysId command
-     */
-    public Command sysIdQuasistatic(SysIdRoutine.Direction direction, Angle lowerLimit, Angle upperLimit) {
-        return sysIdRoutime.quasistatic(direction);
-    }
-
     /**
      * Create a Dynamic SysId command
      * @param direction direction of the command
