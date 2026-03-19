f70be2dc8122daf2760063a4f752da199ca472fc
Ryan Fitz-Gerald <ztif33@gmail.com>
2026-03-19 13:19:52 -0400

    Fixed Intake Angle Merge

--- src/main/java/frc/robot/subsystems/IntakeAngle.java
+++ src/main/java/frc/robot/subsystems/IntakeAngle.java
@@ -26,7 +26,6 @@ import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import au.grapplerobotics.LaserCan;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Time;
@@ -95,12 +94,6 @@ public class IntakeAngle extends SubsystemBase {
     private final TalonFX motor;
     private final CANcoder encoder;
 
-    private final LaserCan leftLaserCan;
-    private final LaserCan rightLaserCan;
-
-    private final MutDistance leftHopperLevel = Millimeters.mutable(0);
-    private final MutDistance rightHopperLevel = Millimeters.mutable(0);
-
     // Motor Control Requests
     private final VoltageOut voltCtrl = new VoltageOut(0.0);
     private final MotionMagicVelocityVoltage velCtrl = new MotionMagicVelocityVoltage(0)
@@ -131,17 +124,9 @@ public class IntakeAngle extends SubsystemBase {
      * 
      * @param motorID
      */
-    public IntakeAngle(
-            int motorId,
-            int encoderId,
-            int leftLaserCanID,
-            int rightLaserCanID,
-            CANBus bus,
-            double gearRatio) {
+    public IntakeAngle(int motorId, int encoderId, CANBus bus, double gearRatio) {
         motor = new TalonFX(motorId, bus);
         encoder = new CANcoder(encoderId, bus);
-        leftLaserCan = new LaserCan(leftLaserCanID);
-        rightLaserCan = new LaserCan(rightLaserCanID);
 
         motorConfig.MotorOutput
                 .withNeutralMode(NeutralModeValue.Brake)
@@ -175,13 +160,6 @@ public class IntakeAngle extends SubsystemBase {
                 .withMotionMagicCruiseVelocity(RotationsPerSecond.of(6))
                 .withMotionMagicAcceleration(RotationsPerSecondPerSecond.of(12))
                 .withMotionMagicJerk(120);
-
-        // TODO Set intake position limits
-        motorConfig.SoftwareLimitSwitch
-                .withForwardSoftLimitEnable(true)
-                .withForwardSoftLimitThreshold(Degrees.of(90))
-                .withReverseSoftLimitEnable(true)
-                .withReverseSoftLimitThreshold(Degrees.of(0));
 
         motor.getConfigurator().apply(motorConfig);
 
@@ -252,63 +230,6 @@ public class IntakeAngle extends SubsystemBase {
     @AutoLogOutput
     public Angle getPosition() {
         return motor.getPosition().getValue();
-    }
-
-    /**
-     * Gets the hopper level from the left sensor
-     * 
-     * @return hopper level from the left sensor
-     */
-    @AutoLogOutput
-    public Distance getLeftHopperLevel() {
-        return leftHopperLevel.mut_replace(leftLaserCan.getMeasurement().distance_mm, Millimeters);
-    }
-
-    /**
-     * Gets the hopper level from the right sensor
-     * 
-     * @return hopper level from the right sensor
-     */
-    @AutoLogOutput
-    public Distance getRightHopperLevel() {
-        return rightHopperLevel.mut_replace(rightLaserCan.getMeasurement().distance_mm, Millimeters);
-    }
-
-    /**
-     * Gets the maximum hopper level value
-     * 
-     * @return maximum hopper level value
-     */
-    @AutoLogOutput
-    public Distance getMaxHopperLevel() {
-        var leftValue = getLeftHopperLevel();
-        var rightValue = getRightHopperLevel();
-
-        return leftValue.gt(rightValue) ? leftValue : rightValue;
-    }
-
-    /**
-     * Check if the hopper is below a threshold
-     * 
-     * @param threshold low hopper threshold
-     * @return True if the hopper is below a threshold
-     */
-    public boolean isHooperLow(Distance threshold) {
-        return getMaxHopperLevel().gt(threshold);
-    }
-
-    /**
-     * Retracts the intake at a set velocity when a condition is true
-     * 
-     * @param retract    True to retract the intake, false otherwise
-     * @param retractVel Retract velocity
-     */
-    public void autoRetract(boolean retract, AngularVelocity retractVel) {
-        if (retract) {
-            setVelocity(retractVel);
-        } else {
-            setVelocity(RotationsPerSecond.zero());
-        }
     }
 
     /**
@@ -403,22 +324,6 @@ public class IntakeAngle extends SubsystemBase {
 
                 setOscilateCmd(amplitude, Degrees.of(60), period)
                         .onlyWhile(() -> testValue.getAsDouble() >= 2));
-    }
-
-    /**
-     * Craetes a new command to automatically retract the intake when the hopper is
-     * low
-     * 
-     * @param lowHopperThresh threshold for a low hopper
-     * @param retractVel      velocity to retract the intake
-     * @return new command to automatically retract the intake when the hopper is
-     *         low
-     */
-    public Command autoRetractCmd(Distance lowHopperThresh, AngularVelocity retractVel) {
-        return Commands.deadline(
-                Commands.waitUntil(() -> motor.getPosition().getValue().gt(Constants.intakeAutoRetractLimit)),
-                this.runEnd(() -> autoRetract(isHooperLow(lowHopperThresh), Constants.intakeAutoRetractVel),
-                        () -> setVelocity(RotationsPerSecond.zero())));
     }
 
     /**
