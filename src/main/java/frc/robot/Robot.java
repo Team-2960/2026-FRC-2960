135f9048dee0f2780b19b7ff76e285065e13429a
han-yes <hangyulheaven@gmail.com>
2026-03-30 06:16:04 -0400

    Created PointToPoint Auton Support Created Waypoint Factory for point to point autons Created more GoToPoint Commands for PointToPoint

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -73,7 +73,7 @@ public class Robot extends LoggedRobot {
 
     @Override
     public void autonomousInit() {
-        //m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+        // m_autonomousCommand = robotContainer.getP2PAutononomousCmd();
         m_autonomousCommand = robotContainer.getAutonomousCommand();
 
         if (m_autonomousCommand != null) {
